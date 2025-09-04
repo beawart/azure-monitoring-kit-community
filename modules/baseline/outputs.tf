be04f31fc99a74eb5e370ad5842f84296507eceb
tahaB <itaha@live.com>
2025-09-05 00:04:38 +1000

    update

--- modules/baseline/outputs.tf
+++ modules/baseline/outputs.tf
@@ -2,14 +2,14 @@
 # Outputs for metric-alert
 #############################################
 
-output "id" {
-  description = "The ID of the created resource"
-  value       = azurerm_<service>.this.id
-}
+# output "id" {
+#   description = "The ID of the created resource"
+#   value       = azurerm_<service>.this.id
+# }
 
-output "name" {
-  description = "The name of the created resource"
-  value       = azurerm_<service>.this.name
-}
+# output "name" {
+#   description = "The name of the created resource"
+#   value       = azurerm_<service>.this.name
+# }
 
 # Add more outputs as needed
