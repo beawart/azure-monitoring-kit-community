1473338812acb14263aab514cae97f6932c7f123
tahaB <itaha@live.com>
2025-09-16 01:15:03 +1000

    update

--- modules/baseline/locals.tf
+++ modules/baseline/locals.tf
@@ -31,37 +31,5 @@ locals {
         }
       ]
     }
-
-    # 3️⃣ Network Rule Change (Activity Log)
-    network_rule_change = {
-      scope_id    = var.target_resource_id
-      description = "Storage account network rules updated"
-      severity    = 2
-      criteria = [
-        {
-          metric_namespace = "microsoft.insights/activitylogs"
-          metric_name      = "StorageAccountNetworkRuleSetUpdated"
-          aggregation      = "Count"
-          operator         = "GreaterThan"
-          threshold        = 0
-        }
-      ]
-    }
-
-    # 4️⃣ Key Regeneration (Activity Log)
-    key_regeneration = {
-      scope_id    = var.target_resource_id
-      description = "Storage account access key regenerated"
-      severity    = 2
-      criteria = [
-        {
-          metric_namespace = "microsoft.insights/activitylogs"
-          metric_name      = "RegenerateKey"
-          aggregation      = "Count"
-          operator         = "GreaterThan"
-          threshold        = 0
-        }
-      ]
-    }
   }
 }
